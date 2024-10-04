49edd55b8139c0629b8b859fa2f35def26aae016
isaac <130880666+isaac123451@users.noreply.github.com>
2024-10-04 05:54:01 -0300

    .

--- myapp/src/main.jsx
+++ myapp/src/main.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar'
-import Home from './components/Home';
+import { BrowserRouter } from 'react-router-dom';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
